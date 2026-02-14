1ea87abf476b3253d3c49996c8875111e16c7ad4
Ted Roberts <tedr@microsoft.com>
2026-02-13 18:03:33 -0800

    Tweak the bottom buttons

--- dist/HA-Firemote.js
+++ dist/HA-Firemote.js
@@ -5576,20 +5576,22 @@ class FiremoteCard extends LitElement {
           <button class="remote-button" id="back-button" @pointerdown=${this.buttonDown}>
             <ha-icon icon="mdi:arrow-left"></ha-icon>
           </button>
+          <button class="remote-button${homeStatusClass}" id="home-button" @pointerdown=${this.buttonDown}>
+            <ha-icon icon="mdi:home"></ha-icon>
+          </button>
+
           <button class="remote-button${playingStatusClass}" id="playpause-button" @pointerdown=${this.buttonDown}>
             <ha-icon icon="mdi:play-pause"></ha-icon>
           </button>
-
           <button class="remote-button" id="rewind-button" @pointerdown=${this.buttonDown}>
             <ha-icon icon="mdi:rewind"></ha-icon>
           </button>
+
+          <div></div>
           <button class="remote-button" id="fastforward-button" @pointerdown=${this.buttonDown}>
             <ha-icon icon="mdi:fast-forward"></ha-icon>
           </button>
 
-          <div></div>
-          <div></div>
-          
           ${drawAppLaunchButtons(this, this._config, 2, appButtonMax["KA1"])}
 
           ${drawDeviceName(this, this._config, 'bottom')}
